5b078fd2ee5717277269fefa6ddf00b5ed68c397
Alberto <42894888+Nenino2@users.noreply.github.com>
2022-10-22 17:28:33 +0200

    fix sync issue

--- packages/shell/src/bootstrap.ts
+++ packages/shell/src/bootstrap.ts
@@ -9,9 +9,12 @@ const globalEventDistributor: GlobalEventDistributor = new GlobalEventDistributo
  * Register applications here
  */
 
-registerApplication("portal", () => import("portal/App"), () => true, { globalEventDistributor },);
-registerApplication("app1", () => import("app1/ApplicationPage"), (location: Location) => location.pathname.startsWith("/angular"), { globalEventDistributor },);
-registerApplication("app2", () => import("app2/ApplicationPage"), (location: Location) => location.pathname.startsWith("/react"), { globalEventDistributor },);
-registerApplication("app3", () => import("app3/ApplicationPage"), (location: Location) => location.pathname.startsWith("/vue"), { globalEventDistributor },);
+registerApplication("portal", async () => {
+    const portal = await import("portal/App");
+    registerApplication("app1", () => import("app1/ApplicationPage"), (location: Location) => location.pathname.startsWith("/angular"), { globalEventDistributor },);
+    registerApplication("app2", () => import("app2/ApplicationPage"), (location: Location) => location.pathname.startsWith("/react"), { globalEventDistributor },);
+    registerApplication("app3", () => import("app3/ApplicationPage"), (location: Location) => location.pathname.startsWith("/vue"), { globalEventDistributor },);
+    return portal;
+}, () => true, { globalEventDistributor },);
 
 start();
